ccabbae1739fed5e505964b27d28231df4e822f8
Raph Cohn <raphael.cohn@stormmq.com>
2016-03-24 17:35:40 +0000

    Going faster

--- source/java-parsing-fileParsers/com/stormmq/java/parsing/fileParsers/FileParser.java
+++ source/java-parsing-fileParsers/com/stormmq/java/parsing/fileParsers/FileParser.java
@@ -8,19 +8,6 @@ import java.util.zip.ZipFile;
 
 public interface FileParser
 {
-	@NotNull FileParser DoNothingFileParser = new FileParser()
-	{
-		@Override
-		public void parseFile(@NotNull final Path filePath, @NotNull final Path sourceRootPath)
-		{
-		}
-
-		@Override
-		public void parseFile(@NotNull final ZipFile zipFile, @NotNull final ZipEntry zipEntry)
-		{
-		}
-	};
-
 	void parseFile(@NotNull final Path filePath, @NotNull final Path sourceRootPath);
 
 	void parseFile(@NotNull final ZipFile zipFile, @NotNull final ZipEntry zipEntry);
